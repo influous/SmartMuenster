00e6ee66a453587fa71afd718565ec45981da752
Tobias Weber <rodion.kovalenko@birkle-it.com>
2020-01-27 22:16:52 +0100

    push project

---
+++ SmartMuenster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartMuenster
+//
+//  Created by Tobias on 27.01.20.
+//  Copyright © 2020 Tobias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! This is a test.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
